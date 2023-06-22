41eb68c21f71b3a7acd179156acb1e0c5d554f39
ghousalik97 <ghousali11@gmail.com>
2023-06-22 18:57:57 +0800

    home and list page done

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,9 @@
+import { Add, Remove } from "@mui/icons-material";
 import Announcement from "../components/announcement/Announcement";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 import Newsletter from "../components/newsletter/Newsletter";
-
+import './product.css';
 
 function Product() {
     return (
@@ -10,9 +11,44 @@ function Product() {
             <Announcement />
             <Navbar />
             <div className="productContainer">
-                <div className="imgContainer"></div>
-                <div className="infoContainer"></div>
-                <div className="filterContainer"></div>
+                <div className="imgContainer">
+                    <img src="https://i.ibb.co/S6qMxwr/jean.jpg" alt="" />
+                </div>
+                <div className="infoContainer">
+                    <h1>
+                        Denim jumpsuit
+                    </h1>
+                    <p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                        venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+                        iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+                        tristique tortor pretium ut. Curabitur elit justo, consequat id
+                        condimentum ac, volutpat ornare.</p>
+                    <h3>$ 20</h3>
+                    <div className="filterContainer">
+                        <div className="filter">
+                            <span>Color</span>
+                        </div>
+                        <div className="filter">
+                            <span>Size</span>
+                            <select>
+                                <option>XS</option>
+                                <option>S</option>
+                                <option>M</option>
+                                <option>L</option>
+                                <option>XL</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="addContainer">
+                        <div className="amountContainer">
+                            <Add />
+                            <span>1</span>
+                            <Remove />
+                        </div>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+
             </div>
             <Newsletter />
             <Footer />
